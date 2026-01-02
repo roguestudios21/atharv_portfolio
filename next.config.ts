51029a852fc8c356e1707b08aaaf434b7010151f
roguestudios21 <atharv.choughule@gmail.com>
2026-01-02 20:08:27 +0530

    1st deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/atharv_portfolio",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
