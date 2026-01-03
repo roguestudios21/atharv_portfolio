bac1e8cdbdd21e2a9366babbf4c42e3b675c03b2
roguestudios21 <atharv.choughule@gmail.com>
2026-01-03 14:12:29 +0530

    deployment 1.0.4

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const isGithubActions = process.env.GITHUB_ACTIONS === "true";
-const isCloudflare = process.env.CF_PAGES === "1";
 const basePath = isGithubActions ? "/atharv_portfolio" : "";
 
 const nextConfig: NextConfig = {
